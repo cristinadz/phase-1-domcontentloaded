b81ae62312a98f8b1275f55dae74331f715da180
+ <diazcristina.la@gmail.com>
2022-05-02 15:49:15 -0700

    commit tree d217feefc484b6804b4b94451c0a04b71d6f835a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const paragraphText = document.getElementById("text");
+  paragraphText.textContent = "This is really cool!";
+});
+
+console.log("Before DOM loads");
